1d7a6f36d699e24a686bb201fefdaba0b941037a
Matthew Gimbl <matthew.gimbl@gmail.com>
2024-05-17 10:02:18 -0500

    initial commit

--- src/app/page.js
+++ src/app/page.js
@@ -8,15 +8,10 @@ import Verify from "@/app/verify";
 export default function Home() {
 
   return (
-
           <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
-
                   <Suspense fallback={<>Loading...</>}>
                       <Verify></Verify>
                   </Suspense>
-
-
           </main>
   )
 }
